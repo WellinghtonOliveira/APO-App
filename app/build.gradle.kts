1a6bc7eff0395ba1312983c8fb2cdae6b5b3c7f4
Wellinghton O. <71161234+WellinghtonOliveira@users.noreply.github.com>
2025-11-08 23:14:05 -0300

    Correções e Implementações

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.weatherapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36  // Corrigido a versão do compileSdk
 
     defaultConfig {
         applicationId = "com.example.weatherapp"
@@ -20,16 +18,18 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = false  // Mantenha false ou altere para true, se desejar usar Proguard
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        //compilerArgs = listOf("-Xlint:deprecation")  // Adicionando a flag para identificar APIs descontinuadas
     }
 }
 
@@ -41,19 +41,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    // app/build.gradle.kts -> dentro de dependencies { ... }
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.9.0")
+
+    // Removendo dependências duplicadas, já importadas via alias "libs"
     implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.github.bumptech.glide:glide:4.15.1")
-    //kapt("com.github.bumptech.glide:compiler:4.15.1") // se usar annotationProcessor substitua por kapt se necessário
+    //kapt("com.github.bumptech.glide:compiler:4.15.1")  // Descomente se for usar anotações com Glide
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("androidx.fragment:fragment-ktx:1.6.0")
-
 }
